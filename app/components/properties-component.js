f62f62edbfe1cfbfcf576b7c7b38f3915c2c3638
hossam <rashwan@hossams-MBP.lan>
2018-05-20 15:08:34 +0200

    delete units and delete properties and fix issue ui

--- app/components/properties-component.js
+++ app/components/properties-component.js
@@ -1,5 +1,5 @@
 import Component from '@ember/component';
-
+import swal from 'sweetalert';
 export default Component.extend({
     tableLoading: true,
     didInsertElement() {
@@ -11,17 +11,50 @@ export default Component.extend({
             }
         );
     },
-    actions:{
-        rentAction(){
-            
+    actions: {
+        deleteProperty(id) {
+           var self = this;
+            swal({
+                title: "هل أنت متأكد",
+                text: "لايمكنك استرجاع هذا العقار",
+                type: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#DD6B55",
+                confirmButtonText: "نعم",
+                cancelButtonText: "إلغاء",
+                closeOnConfirm: true,
+                closeOnCancel: true
+            },
+                function (isConfirm) {
+                    if (isConfirm) {
+                        new Ember.RSVP.Promise(function (resolve, reject) {
+                            self.manager.ajaxRequest(self, self.get('urls').updateProperty(id)+ "/destroy", 'DELETE', resolve, reject);
+                        }).then(
+                            success => {
+                               let property =  self.store.peekRecord('mproperty', id);
+                               self.store.unloadRecord(property);
+                               self.manager.toaster(self, 'تم حذف العقار بنجاح   ')
+
+                            },
+                            errors => {
+                                console.log(errors)
+                            }
+                        )
+                    }
+                })
+
+
         },
-        propertyView(id){
+        rentAction() {
+
+        },
+        propertyView(id) {
             this.get('router').transitionTo('index.properties.show.property-data', id)
         },
-        editProperty(id){
+        editProperty(id) {
             this.get('router').transitionTo('index.properties.edit.status', id)
         },
-        addProperty(){
+        addProperty() {
             this.get('router').transitionTo('index.properties.add.property-data')
         }
     }
--- app/components/units-component.js
+++ app/components/units-component.js
@@ -8,6 +8,33 @@ export default Component.extend({
         rentAction(){
             
         },
+        deleteUnit(id) {
+            var self = this;
+             swal({
+                 title: "هل أنت متأكد",
+                 text: "لايمكنك استرجاع هذة الوحدة",
+                 type: "warning",
+                 showCancelButton: true,
+                 confirmButtonColor: "#DD6B55",
+                 confirmButtonText: "نعم",
+                 cancelButtonText: "إلغاء",
+                 closeOnConfirm: true,
+                 closeOnCancel: true
+             },
+                 function (isConfirm) {
+                     if (isConfirm) {
+                        self.store.findRecord('unit', id, { backgroundReload: false }).then(function(unit) {
+                            unit.deleteRecord();
+                            unit.get('isDeleted');
+                            unit.save(); 
+                            self.manager.toaster(self, 'تم حذف الوحدة بنجاح   ')
+                          });
+                         
+                     }
+                 })
+ 
+ 
+         },
         unitView(id){
             this.get('router').transitionTo('index.properties.show.units.unit-show', id)
         },
